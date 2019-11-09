b25dbc0e2fb404460b8b8431afdc20f94b580802
DrNykterstein <46200971+DrNykterstein@users.noreply.github.com>
2019-11-08 23:12:46 -0400

    Nuevas cosas

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700&display=swap" rel="stylesheet">
     <title>VBlog</title>
 </head>
 <body>
@@ -12,7 +13,7 @@
     <header class="site-header">
         <div class="contenedor contenedor-flex"><!--Contenedor flex para flexbox -->
             <a href="index.php">
-                <h1>Skeleton</h1>
+                <h1>Skele<span>ton</span></h1>
             </a>
             <!--Nav-->
             <div class="navegacion">
@@ -32,6 +33,12 @@
     <div class="contenedor contenido-principal"><!--Contenedor padre, para flexbox -->
         <!--Formulario para el login -->
         <aside class="sidebar">
+            <div class="buscardor">
+                <form action="">
+                    <input type="search" placeholder="Buscar Entrada" class="block-aside">
+                </form>
+            </div><!--buscador -->
+
             <div class="login block-aside"><!--Para loguearse -->
                 <h3>Identificate</h3>
                 <form action="login.php" method="POST"><!--Llevame a login.php -->
@@ -51,30 +58,37 @@
                     <input type="submit" value="Registrarse">
                 </form>
             </div><!--registrer -->
+
         </aside><!--sidebar -->
 
         <!--Contenido principal-->
         <main class="entrada-blog">
             <h1>Últimas Entradas</h1>
             <article class="entrada">
+                <img src="assets/img/guerra1.png" alt="" >
                 <h2>VideoJuego de Guerra</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus ut
                  laudantium quidem iusto, sed officia nam recusandae, natus officiis reprehenderit 
                  quo vitae inventore cumque expedita consectetur omnis dolor sint.</p>
+                 <a href="entrada.php" class="boton boton-principal">Leer Entrada</a>
             </article>
 
             <article class="entrada">
+                <img src="assets/img/futbol1.png" alt="">
                 <h2>VideoJuego de NBA</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus ut
                  laudantium quidem iusto, sed officia nam recusandae, natus officiis reprehenderit 
                  quo vitae inventore cumque expedita consectetur omnis dolor sint.</p>
+                 <a href="entrada.php" class="boton boton-principal">Leer Entrada</a>
             </article>
 
             <article class="entrada">
+                <img src="assets/img/guerra1.png" alt="">
                 <h2>VideoJuego de Aventura</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus ut
                  laudantium quidem iusto, sed officia nam recusandae, natus officiis reprehenderit 
                  quo vitae inventore cumque expedita consectetur omnis dolor sint.</p>
+                 <a href="entrada.php" class="boton boton-principal">Leer Entrada</a>
             </article>
         </main><!--Contenido principal -->
 
@@ -83,7 +97,15 @@
 
     <!-- Pie de pagina -->
     <footer class="pie-pagina">
-        <p>Desarrollado por el Equipo Cryptogami </p>
+        <div class="contenedor">
+            <div class="barra">
+                <p>Ske<span>leton</span></p>
+                <nav class="navegacion">
+                    <a href="#">Inicio</a>
+                    <a href="#">Contacto</a>
+                </nav>
+            </div>
+        </div>
     </footer>
 
 </body>
